04428052d4e2cb0194f2d06d9b27c38ccf06f276
Mehdizaiem <Mehdizaiem@users.noreply.github.com>
2025-12-09 16:36:22 +0100

    addtion chap 14 / 15

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -6,6 +6,10 @@ from django.test import TestCase
 from django.contrib.auth import get_user_model
 from core import models
 
+def create_user(email='user@example.com',password='testpass123'):
+    """Create and return a new user."""
+    return get_user_model().objects.create_user(email=email, password=password)
+
 class ModelTests(TestCase):
     """Test Models"""
 
@@ -81,3 +85,18 @@ class ModelTests(TestCase):
             description='Sample recipe description.',
         )
         self.assertEqual(str(recipe), recipe.title)
+    def test_create_tag(self):
+        """Test creating a tag is successful"""
+        user = create_user()
+        tag = models.Tag.objects.create(user=user, name='tag1')
+
+        self.assertEqual(str(tag), tag.name)
+    def test_create_ingredient(self):
+        """Test creating an ingredient is successful."""
+        user = create_user()
+        ingredient = models.Ingredient.objects.create(
+            user=user,
+            name='Ingredient1'
+        )
+
+        self.assertEqual(str(ingredient), ingredient.name)
